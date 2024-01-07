3a2afcad9af17562f98231707ed3d3be24c86fd4
RaY-MbS60001 <114419971+RaY-MbS60001@users.noreply.github.com>
2024-01-07 10:43:49 +0200

    Add files via upload

---
+++ functioning.js
@@ -0,0 +1,15 @@
+/*three rams function transform*/
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+/*code to display the navigation bar*/
+function myFunction() {
+    var x = document.getElementById("navLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }
+     else {
+      x.style.display = "block";
+    }
+}
